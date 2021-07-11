2563dfb89993eb029c71682268900919e54efd6c
Gabriel Bogni <gabriel96kt@gmail.com>
2021-07-11 16:20:03 -0300

    Configurações de Hospedagem

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    url: "mongodb://localhost:27017/trabtc2"
+    url: "mongodb+srv://SC3002519:senha@cluster0.gt538.mongodb.net/SC3002519_trabalho_bd?retryWrites=true&w=majority"
 }
 
